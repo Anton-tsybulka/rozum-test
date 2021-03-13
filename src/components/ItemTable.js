16fbd2fe3b40a9fbb0d4d9d73f80a8826d107764
Anton Tsybulka <anton.kurto@gmail.com>
2021-03-13 18:25:36 +0300

    refactoring

--- src/components/ItemTable.js
+++ src/components/ItemTable.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ItemTable = ({ filterById }) => {
-  const renderItem = filterById.map(({employee_id, from, to, id}) => {
-    return(
-      <tr key={id}>
-        <td>{employee_id}</td>
-        <td>{from}</td>
-        <td>{to}</td>
-      </tr>
-    )
-  })
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Прибыл</th>
-            <th>Убыл</th>
-          </tr>
-        </thead>
-        <tbody>
-          {renderItem}
-        </tbody>
-      </table>
-      <button style={{marginTop: '1em', marginLeft: '2em'}}><Link to='/' style={{textDecoration: 'none'}}>Назад</Link></button>
-    </div> 
-  );
+const ItemTable = ({ worklog, id }) => {
+    const renderItem = 
+        worklog &&
+        worklog.length !== 0 &&
+        worklog.filter(({employee_id}) => employee_id === id)
+            .map(({employee_id, from, to, id}) => {
+                return(
+                  <tr key={id}>
+                    <td>{employee_id}</td>
+                    <td>{from}</td>
+                    <td>{to}</td>
+                  </tr>
+                )
+              });
+
+    return (
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Прибыл</th>
+                    <th>Убыл</th>
+                </tr>
+                </thead>
+                <tbody>
+                {renderItem}
+                </tbody>
+            </table>
+            <button style={{marginTop: '1em', marginLeft: '2em'}}><Link to='/' style={{textDecoration: 'none'}}>Назад</Link></button>
+        </div> 
+    );
 }
 
 export default ItemTable;
